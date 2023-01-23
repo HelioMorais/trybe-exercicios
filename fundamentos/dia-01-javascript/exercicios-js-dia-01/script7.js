cfa0bad2e03ae394e909510b28c10211e3c7dea0
Helio Morais <heliogov20@gmail.com>
2023-01-23 17:46:29 -0300

    Praticando JScript parte aula ao vivo

---
+++ fundamentos/dia-01-javascript/exercicios-js-dia-01/script7.js
@@ -0,0 +1,12 @@
+let  num1 = 7
+let  num2 = 21
+let resultado = 0
+if (num1 === num2) {
+    resultado = num1 + num2;
+    console.log(resultado);
+}
+else {
+    resultado = num1 * num2;
+    console.log(resultado);
+}
+
---
+++ fundamentos/dia-01-javascript/exercicios-js-dia-01/script8.js
@@ -0,0 +1,9 @@
+const num1 = 10;
+const num2 = 15;
+
+if (num1 > num2) {
+    console.log("O  numero maior é: " +  num1);
+}
+else  {
+    console.log("O numero maior é:" + num2);
+}
