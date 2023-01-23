832e43a72fe0ff41f82b4436d1d33406ade04c23
Helio Morais <heliogov20@gmail.com>
2023-01-23 15:54:26 -0300

    Praticando JScript parte 3

---
+++ fundamentos/dia-01-javascript/exercicios-js-dia-01/script3.js
@@ -0,0 +1,11 @@
+const nota = 100;
+
+if (nota >= 80) {
+console.log("Parabéns, você faz parte do grupo das pessoas aprovadas!");
+}
+else if (nota < 80 && nota >= 60) {
+    console.log("Você está na nossa lista de espera");
+}
+else if (nota < 60) {
+    console.log("Infelizmente, você reprovou");
+}
