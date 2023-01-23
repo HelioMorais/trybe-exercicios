76085f41fd348eab0bf526142e9fc3ad7a95da22
Helio Morais <heliogov20@gmail.com>
2023-01-23 16:13:43 -0300

    Praticando JScript parte 5

---
+++ fundamentos/dia-01-javascript/exercicios-js-dia-01/script5.js
@@ -0,0 +1,27 @@
+const currentHour = 7;
+let message = "";
+
+if (currentHour >= 22) {
+    message = "Não deveríamos comer nada, é hora de dormir";
+}
+else  if (currentHour >= 18 && currentHour < 22) {
+    message = "Rango da noite, vamos jantar :D";
+}
+else if (currentHour >= 14 && currentHour < 18) {
+    message =  "Vamos fazer um bolo para o café da tarde?";
+}
+else if (currentHour >= 11 && currentHour <14) {
+    message = "Hora do almoço!!!";
+}
+else if (currentHour >= 4 && currentHour <=11) {
+    message = "Hmmmmmmm, cheiro de café recém-passado";
+}
+
+
+const weekDay = "quarta-feira";
+
+if (weekDay === "segunda-feira" || weekDay === "terça-feira" || weekDay === "quarta-feira" || weekDay === "quinta-feira" || weekDay === "sexta-feira") {
+    console.log("Oba, mais um dia de aprendizado na Trybe >:D");
+   } else {
+     console.log("FINALMENTE, descanso merecido UwU");
+   }
