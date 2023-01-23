7c285a0316bb0c5ffcb2b1b95320341dbc71f915
Helio Morais <heliogov20@gmail.com>
2023-01-23 15:16:35 -0300

    Praticando JScript

---
+++ fundamentos/dia-01-javascript/exercicios-js-dia-01/script.js
@@ -0,0 +1,11 @@
+const myName = "Carolina";
+console.log(myName);
+
+const birthCity = "Governador Valadares";
+console.log(birthCity);
+birthCity = "Vancouver";
+
+let birthYear = 1996;
+birthYear = 2030;
+console.log(birthYear);
+
