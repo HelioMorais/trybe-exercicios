adcda23a824e1a3f25048f8098850fbd2c416277
Helio Morais <heliogov20@gmail.com>
2023-01-24 16:10:02 -0300

    Praticando JavaScript no dia 3.2

--- fundamentos/dia-02-javascript/script6.js
+++ fundamentos/dia-02-javascript/script6.js
@@ -2,3 +2,12 @@ let names = ['João', 'Maria', 'Antônio', 'Margarida'];
 for (let nome of names) {
     console.log(nome);   
 }
+
+
+for(let index = 0; index < 10; index+=1 ){
+    console.log(index);
+  }
+  
+  for(let index = 0; index < 9999; index+=1 ){
+    console.log('Churrasco com cerveja gelada :D');}
+  
