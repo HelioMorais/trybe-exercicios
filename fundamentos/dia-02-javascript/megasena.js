f7d0aa074fbffa3ad66b68950a79ff134ce79882
Helio Morais <heliogov20@gmail.com>
2023-01-24 17:09:38 -0300

    Criando os numeros aleatorios da mega sena

---
+++ fundamentos/dia-02-javascript/megasena.js
@@ -0,0 +1,13 @@
+let primeiraJogada = [7,12,25,36,18,20];
+let megaSena = [48,14,15,28,34,8];
+
+let primeiroNumeroSorteado = Math.floor(Math.random() * 60) + 1;
+let segundoNumeroSorteado = Math.floor(Math.random() * 60) + 1;
+let terceiroNumeroSorteado = Math.floor(Math.random() * 60) + 1;
+let quartoNumeroSorteado = Math.floor(Math.random() * 60) + 1;
+let quintoNumeroSorteado = Math.floor(Math.random() * 60) + 1;
+let sextoNumeroSorteado = Math.floor(Math.random() * 60) + 1;
+
+let megaSenaNumeros = [primeiroNumeroSorteado, segundoNumeroSorteado, terceiroNumeroSorteado, quartoNumeroSorteado, quintoNumeroSorteado, sextoNumeroSorteado];
+
+console.log(megaSenaNumeros);
