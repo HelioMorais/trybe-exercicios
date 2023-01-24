98fff5057aae4eef9f5ff8f37e026276762cf0ea
Helio Morais <heliogov20@gmail.com>
2023-01-24 18:16:58 -0300

    Criando a  mega sena

--- fundamentos/dia-02-javascript/megasena.js
+++ fundamentos/dia-02-javascript/megasena.js
@@ -1,5 +1,5 @@
-let primeiraJogada = [7,12,25,36,18,20];
-let megaSena = [48,14,15,28,34,8];
+let primeiraJogada = [7, 12, 25, 36, 18, 20];
+let megaSena = [48, 14, 15, 28, 34, 8];
 
 let primeiroNumeroSorteado = Math.floor(Math.random() * 60) + 1;
 let segundoNumeroSorteado = Math.floor(Math.random() * 60) + 1;
@@ -10,4 +10,29 @@ let sextoNumeroSorteado = Math.floor(Math.random() * 60) + 1;
 
 let megaSenaNumeros = [primeiroNumeroSorteado, segundoNumeroSorteado, terceiroNumeroSorteado, quartoNumeroSorteado, quintoNumeroSorteado, sextoNumeroSorteado];
 
-console.log(megaSenaNumeros);
+
+for (let index = 0; index < primeiraJogada.length; index += 1) {
+    console.log("Numero da minha jogada: " + primeiraJogada[index]);
+
+}
+
+//for (let indexMegaSena = 0; indexMegaSena < megaSenaNumeros.length; indexMegaSena += 1) {
+   // console.log("Numero da jogada" + megaSenaNumeros[indexMegaSena]);
+
+//}
+
+//comparação 
+let contagemNumeroSorteados = 0;
+for (let indexMegaSena = 0; indexMegaSena < megaSenaNumeros.length; indexMegaSena += 1) {
+    for (let index = 0; index < primeiraJogada.length; index += 1) {
+       if (megaSenaNumeros[indexMegaSena] === primeiraJogada[index]) {
+        console.log("deu certo");
+        contagemNumeroSorteados = contagemNumeroSorteados + 1;
+       }    
+    }
+}
+
+console.log("Numero sorteados: " + megaSenaNumeros);
+console.log("========================================");
+console.log("Jogada  1");
+console.log("");
