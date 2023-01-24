cac855f331981a1a7c0e2e253b021cd5488210bd
Helio Morais <heliogov20@gmail.com>
2023-01-24 19:15:50 -0300

    Exercicios de  JavaScript

--- fundamentos/dia-02-javascript/script13.js
+++ fundamentos/dia-02-javascript/script13.js
@@ -6,3 +6,10 @@ for (let index = 1; index <= 25; index += 1) {
 }
 
 console.log(numbers);
+
+// Utilizando o array que acabou de criar, imprima o resultado da divisão de cada um dos elementos por 2.
+
+for (let index = 0; index < numbers.length; index += 1) {
+  console.log(numbers[index] / 2);
+  
+}
