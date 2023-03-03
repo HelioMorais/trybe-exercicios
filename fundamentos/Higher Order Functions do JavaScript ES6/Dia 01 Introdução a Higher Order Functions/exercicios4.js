2a4dde76c1ff90e8e29d4999797ab079eec97eb0
Helio Morais <heliogov20@gmail.com>
2023-03-02 22:19:25 -0300

    fazendo exercicios

---
+++ fundamentos/Higher Order Functions do JavaScript ES6/Dia 01 Introdução a Higher Order Functions/exercicios4.js
@@ -0,0 +1,72 @@
+// Encontre o nome da primeira pessoa autora do livro nascida no ano de 1947.
+// De olho na dica 👀: use a função find.
+
+
+const books = [
+    {
+      id: 1,
+      name: 'As Crônicas de Gelo e Fogo',
+      genre: 'Fantasia',
+      author: {
+        name: 'George R. R. Martin',
+        birthYear: 1948,
+      },
+      releaseYear: 1991,
+    },
+    {
+      id: 2,
+      name: 'O Senhor dos Anéis',
+      genre: 'Fantasia',
+      author: {
+        name: 'J. R. R. Tolkien',
+        birthYear: 1892,
+      },
+      releaseYear: 1954,
+    },
+    {
+      id: 3,
+      name: 'Fundação',
+      genre: 'Ficção Científica',
+      author: {
+        name: 'Isaac Asimov',
+        birthYear: 1920,
+      },
+      releaseYear: 1951,
+    },
+    {
+      id: 4,
+      name: 'Duna',
+      genre: 'Ficção Científica',
+      author: {
+        name: 'Frank Herbert',
+        birthYear: 1920,
+      },
+      releaseYear: 1965,
+    },
+    {
+      id: 5,
+      name: 'A Coisa',
+      genre: 'Terror',
+      author: {
+        name: 'Stephen King',
+        birthYear: 1947,
+      },
+      releaseYear: 1986,
+    },
+    {
+      id: 6,
+      name: 'O Chamado de Cthulhu',
+      genre: 'Terror',
+      author: {
+        name: 'H. P. Lovecraft',
+        birthYear: 1890,
+      },
+      releaseYear: 1928,
+    },
+  ];
+  
+  const expectedResult = 'Stephen King';
+  const authorBornIn1947 = () => {
+    return books.find((element) => element.author.birthYear === 1947).author.name;
+  }
+console.log(authorBornIn1947());
